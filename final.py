3863bc34f09662b0ef8101ad9a21354bb00b4584
daneshwar <daneshsgobbani@gmail.com>
2020-10-27 21:14:04 +0530

    pathch media

--- final.py
+++ final.py
@@ -64,10 +64,13 @@ if st.sidebar.checkbox("Login"):
                 Collaborator_Name.append(c[i]["get_event_collaborator"]["name"])
                 Collaborator_Phone.append(c[i]["get_event_collaborator"]["mobile"])
 
-
-                temp=c[i]["media_url"]
-                z=temp[-21:]
-                Media_file.append(z)
+                try:
+                    temp=c[i]["media_url"]
+                    print(temp)
+                    z=temp[-21:]
+                    Media_file.append(z)
+                except:
+                    Media_file.append("None")
 
                 try:
                     Question_chosen_from_suggestion.append(c[i]["meta_data"]["questionSelected"])
@@ -161,10 +164,11 @@ if st.sidebar.checkbox("Login"):
 
 
         fig = make_subplots(
-            rows=1, cols=1,
+            rows=2, cols=1,
             shared_xaxes=True,
             vertical_spacing=0.03,
-            specs=[[{"type": "table"}]]
+            specs=[[{"type": "table"}],
+                    [{"type": "Pie"}]]
         )
 
         fig.add_trace(
@@ -179,6 +183,11 @@ if st.sidebar.checkbox("Login"):
                     align = "left")
             ),
             row=1, col=1
+        )
+        dd=df["Event_id"].value_counts()
+        fig.add_trace(
+            go.Pie(labels = dd.index, values = dd.values,title="Event ID %"),
+            row=2, col=1
         )
         fig.update_layout(
             height=1000,
@@ -197,96 +206,101 @@ if st.sidebar.checkbox("Login"):
 
 
         for j in range(266,267):
-            url = f"https://api.miljulapp.com/api/v1/api/v1/collab/collabmedia?collab_event={j}"
-            response = urllib.request.urlopen(url)
-            text = response.read()
-            json_data = json.loads(text)
-            api_length=json_data["response"]["response"]["count"]
-            c=json_data["response"]["response"]["results"]
-            for i in range(0,len(c)):
-                Collaborator_Name1.append(c[i]["get_event_collaborator"]["name"])
+            counter=1
+            temp=0
+            while(temp != None):
+                url=f"https://api.miljulapp.com/api/v1/api/v1/collab/collaborators?collab_event_id={j}&page={counter}"
+                response = urllib.request.urlopen(url)
+                text = response.read()
+                json_data = json.loads(text)
+                temp=json_data["response"]["response"]["next"]
+                print(temp)
+                counter+=1
+                c=json_data["response"]["response"]["results"]
+                for i in range(0,len(c)):
+                    Collaborator_Name1.append(c[i]["name"])
 
-                Collaborator_Phone1.append(c[i]["get_event_collaborator"]["mobile"])
+                    Collaborator_Phone1.append(c[i]["mobile"])
 
-                Date1.append(c[i]["created"][0:16])
+                    Date1.append(c[i]["created"][0:16])
 
-                try:
-                    Collaborator_Cohort1.append(c[i]["get_event_collaborator"]["meta_data"]["cohort"])
-                except:
-                    Collaborator_Cohort1.append("None")
-
-
-
-
-
-        data = {
-                "Collaborator_Name":Collaborator_Name1,
-                "Collaborator_Phone":Collaborator_Phone1,
-                "Date":Date1,
-                "Collaborator_Cohort":Collaborator_Cohort1
-        }
-        column=[
-                "Collaborator_Name",
-                "Collaborator_Phone",
-                "Date",
-                "Collaborator_Cohort"
-                ]
-
-
-        data = pd.DataFrame (data, columns = column)
-
-        data=data.fillna("None")
-        data["Collaborator_Phone"]= data["Collaborator_Phone"].replace("", "None") 
-
-        data=data.sort_values(by='Date')
-        dff1=data.copy()
+                    try:
+                        Collaborator_Cohort1.append(c[i]["meta_data"]["cohort"])
+                    except:
+                        Collaborator_Cohort1.append("None")
 
 
 
 
 
+            data = {
+                    "Collaborator_Name":Collaborator_Name1,
+                    "Collaborator_Phone":Collaborator_Phone1,
+                    "Date":Date1,
+                    "Collaborator_Cohort":Collaborator_Cohort1
+            }
+            column=[
+                    "Collaborator_Name",
+                    "Collaborator_Phone",
+                    "Date",
+                    "Collaborator_Cohort"
+                    ]
 
-        # filters = st.sidebar.checkbox("Filter")
-        # if filters:
-        # st.sidebar.title("Filter Columns :")
+
+            data = pd.DataFrame (data, columns = column)
+
+            data=data.fillna("None")
+            data["Collaborator_Phone"]= data["Collaborator_Phone"].replace("", "None") 
+
+            data=data.sort_values(by='Date')
+            dff1=data.copy()
 
 
-        # options = pd.Series(["All"]).append(df["Event_name"], ignore_index=True).unique()
-        # choice = st.sidebar.selectbox("Select {}.".format("Event_name."), options)
-
-        # if choice != "All":
-        #     dff = dff[dff["Event_name"] == choice]
 
 
 
-        fig1 = make_subplots(
-            rows=1, cols=1,
-            shared_xaxes=True,
-            vertical_spacing=0.03,
-            specs=[[{"type": "table"}]]
-        )
 
-        fig1.add_trace(
-            go.Table(
-                header=dict(
-                    values=dff1.columns,
-                    font=dict(size=10),
-                    align="left"
+            # filters = st.sidebar.checkbox("Filter")
+            # if filters:
+            # st.sidebar.title("Filter Columns :")
+
+
+            # options = pd.Series(["All"]).append(df["Event_name"], ignore_index=True).unique()
+            # choice = st.sidebar.selectbox("Select {}.".format("Event_name."), options)
+
+            # if choice != "All":
+            #     dff = dff[dff["Event_name"] == choice]
+
+
+
+            fig1 = make_subplots(
+                rows=1, cols=1,
+                shared_xaxes=True,
+                vertical_spacing=0.03,
+                specs=[[{"type": "table"}]]
+            )
+
+            fig1.add_trace(
+                go.Table(
+                    header=dict(
+                        values=dff1.columns,
+                        font=dict(size=10),
+                        align="left"
+                    ),
+                    cells=dict(
+                        values=[dff1[k].tolist() for k in dff1.columns[0:]],
+                        align = "left")
                 ),
-                cells=dict(
-                    values=[dff1[k].tolist() for k in dff1.columns[0:]],
-                    align = "left")
-            ),
-            row=1, col=1
-        )
-        fig1.update_layout(
-            height=1000,
-            width=1000,
-            showlegend=False,
-            title_text="Event Collaborators Data",
-        )
+                row=1, col=1
+            )
+            fig1.update_layout(
+                height=1000,
+                width=1000,
+                showlegend=False,
+                title_text="Event Collaborators Data",
+            )
 
-        st.plotly_chart(fig1)
+            st.plotly_chart(fig1)
 
         # st.write(dff["Download_link"].values)
 
@@ -301,8 +315,12 @@ if st.sidebar.checkbox("Login"):
 
         if st.button('Download Images'):
             for i in dff["Download_link"].values:
+                print(i)
+                if i=="None":
+                    pass
+                else:
 
-                image_url = f"{i}"
+                    image_url = f"{i}"
 
                 st.markdown(download_links(image_url, 'Picture'), unsafe_allow_html=True)
 
